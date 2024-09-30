6d65186fc382d13ed1d495069463d9eb6a3ec31a
Dhanya <dhanyavenugopal20@gmail.com>
2024-09-30 14:54:57 +0530

    learn git

--- sum.c
+++ sum.c
@@ -4,6 +4,6 @@ int main()
 {
   int num1, num2;
   printf("Enter num1, num2 : ");
-  scanf("%d %d", &num1, &num2);
-  printf("sum :%d\n", num1 + num2);
+  scanf("%d %d", &num1, &num2); // reads num1 and num2 from user
+  printf("sum :%d\n", num1 + num2); // finds sum and prints
 }
